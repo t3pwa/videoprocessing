dbab3bc0ada39dfc20b3ea53baf68e4569070c4b
Alexander <alex@fabbing.com>
2023-07-31 06:33:53 +0000

    [TASK] add comment

--- Classes/Converter/LocalFFmpegConverter.php
+++ Classes/Converter/LocalFFmpegConverter.php
@@ -2,6 +2,9 @@
 
 namespace Faeb\Videoprocessing\Converter;
 
+use FFMpeg\FFMpeg;
+use FFMpeg\Coordinate\TimeCode;
+use FFMpeg\Format\Video\X264;
 
 use Faeb\Videoprocessing\Exception\ConversionException;
 use Faeb\Videoprocessing\FormatRepository;
@@ -30,26 +33,28 @@ class LocalFFmpegConverter extends AbstractVideoConverter
     public function process(VideoProcessingTask $task): void
     {
         $localFile = $task->getSourceFile()->getForLocalProcessing(false);
+
+        var_dump(" [process local file] ", $localFile);
+
         $info = $this->ffprobe($localFile);
         $streams = $info['streams'] ?? [];
 
         $duration = $info['format']['duration'] ?? 3600.0;
         $duration = $duration - $task->getConfiguration()['start'] ?? 0;
         $duration = min($duration, $task->getConfiguration()['duration'] ?? INF);
-
         $tempFilename = GeneralUtility::tempnam('video');
+
         try {
             $videoTaskRepository = GeneralUtility::makeInstance(VideoTaskRepository::class);
             $formatRepository = GeneralUtility::makeInstance(FormatRepository::class);
-
             $ffmpegCommand = $formatRepository->buildParameterString($localFile, $tempFilename, $task->getConfiguration(), $streams);
             $progress = $this->ffmpeg($ffmpegCommand);
+
             foreach ($progress as $time) {
                 $progress = $time / $duration;
                 if ($progress > 1.0) {
                     continue;
                 }
-
                 $task->addProgressStep($progress);
                 $videoTaskRepository->store($task);
             }
@@ -57,11 +62,73 @@ class LocalFFmpegConverter extends AbstractVideoConverter
             // make the progress bar end
             $task->addProgressStep(1.0);
             $videoTaskRepository->store($task);
-
             $this->finishTask($task, $tempFilename, $streams);
+            /* **************************************** */
+
+/*
+
+
+            $processedFile = $task->getTargetFile();
+            var_dump($processedFile->getIdentifier());
+            // ***************************************
+            // the php-ffmpeg way, works with t3v11, php82, php-ffmpeg:1.1
+            // https://stackoverflow.com/questions/2043007/generate-preview-image-from-video-file
+            // https://github.com/PHP-FFMpeg/PHP-FFMpeg
+
+            $timeFrame = 3; // seconds from start for frame capture to use as poster image
+
+            // var_dump("try poster, ffmpeg create");
+            $ffmpeg =  FFMpeg::create();
+            // var_dump("after create, try open");
+
+            $video = $ffmpeg->open($localFile);
+            var_dump("after open localfile, try frame");
+
+
+            $frame = $video->frame(TimeCode::fromSeconds($timeFrame));
+            // ->save($extractedImagePath);
+
+            // https://hotexamples.com/examples/ffmpeg.coordinate/TimeCode/-/php-timecode-class-examples.html
+            // https://hotexamples.com/examples/ffmpeg.coordinate/TimeCode/-/php-timecode-class-examples.html
+
+            $poster = $processedFile->getIdentifier();
+            var_dump("posterFilePath processed file", $poster);
+
+            $poster2 = $localFile;
+            var_dump("posterFilePath local file, poster2", $poster2);
+
+            $thumbnail_png = substr_replace($poster , 'png', strrpos($poster , '.') +1);
+            $thumbnail_jpg = substr_replace($poster , 'jpg', strrpos($poster , '.') +1);
+
+            // stripos for last
+
+            $poster_jpg = substr_replace($poster2 , 'jpg', strripos($poster2, '.') +1);
+//            var_dump("pos", strripos( $poster2, '.') );
+            var_dump("thumbnail_png", $thumbnail_png);
+            var_dump("thumbnail_png", $thumbnail_jpg);
+
+
+            $frame->save("/var/www/vhosts/kukurtihar.com/t3v11.kukurtihar.com/public/fileadmin" . $thumbnail_png);
+            $frame->save("/var/www/vhosts/kukurtihar.com/t3v11.kukurtihar.com/public/fileadmin" . $thumbnail_jpg);
+
+            var_dump("poster_jpg", $poster_jpg);
+            $frame->save($poster_jpg);
+
+            var_dump("after save frames");
+            // ********************************************
+
+*/
+
+
+        } catch (Exception $e) {
+            var_dump("poster catch");
+            // $this->logger->notice('poster generation', ['exception' => $e]);
+            throw new \RuntimeException("poster generation not working.");
         } finally {
+            var_dump("finally");
             GeneralUtility::unlink_tempfile($tempFilename);
         }
+
     }
 
     /**
@@ -128,6 +195,9 @@ class LocalFFmpegConverter extends AbstractVideoConverter
         $nice = $this->runner->getCommand('nice');
         if (is_string($nice)) {
             $ffmpeg = "$nice $ffmpeg";
+        } else {
+            var_dump("no nice found, exception?");
+            // throw new \RuntimeException("nice not found.");
         }
 
         $commandStr = "$ffmpeg -loglevel warning -stats $parameters";
