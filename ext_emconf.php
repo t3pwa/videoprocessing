d092a2f4ff44180d129f99e94d6ee55d0476864f
Alexander <alex@fabbing.com>
2023-08-30 10:00:09 +0000

    changed status to alpha, additiona infor, original author

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,8 +6,12 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',
-    'state' => 'stable',
+    'state' => 'alpha',
     'version' => '1.0.0',
+    'description' => 'Automatic conversion of videos within typo3 using local ffmpeg (or cloudconvert). This is a fork of hn/video',
+    'category' => 'fe',
+    'author' => 'A. Reinhard',
+    'author_email' => 'alex@fabbing.com',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
@@ -15,4 +19,11 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
+    'state' => 'alpha',
+    'clearCacheOnLoad' => true,
+    'autoload' => [
+        'psr-4' => [
+            'Faeb\\Videoprocessing\\' => 'Classes',
+        ],
+    ]
 ];
